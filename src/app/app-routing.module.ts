6a3c093a4fbda9431aa9aeaf41d70d8188412592
user <user@docker>
2023-04-28 10:41:27 +0200

    [feature/security] Adding security

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/security/features/login/login.component';
+import { AuthGuard } from './shared/security/guards/auth.guard';
+import { HasRoleGuard } from './shared/security/guards/has-role.guard';
 
 const routes: Routes = [
   {
     path: 'worker',
+    data: { 'role': 'ROLE_WORKER' },
+    canActivate: [AuthGuard, HasRoleGuard],
     loadChildren: () => import('./features').then(m => m.WorkerModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
